d92be322ae7820dfd6988be65f7a63a4eb5b126f
jaydeepgill <jaydeepgill.17@gmail.com>
2016-04-11 01:38:27 -0400

    removed

--- demo/root/js/custom.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    // always check inspect elements > console log 
-    console.log( 'jQuery started.' );
-    
-    $('.subMenu').smint({
-    	'scrollSpeed' : 1000
-    });
-
-});
